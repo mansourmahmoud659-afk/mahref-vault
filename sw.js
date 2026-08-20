ef94f41edbc977209ba11d84c15c524522f758c0
mansourmahmoud659-afk <mansourmahmoud659@gmail.com>
2026-08-20 12:49:02 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,21 @@
+const CACHE_NAME = 'invoice-gen-v1';
 
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll([
+        '/mahref-vault/',
+        '/mahref-vault/index.html',
+        '/mahref-vault/manifest.json'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
