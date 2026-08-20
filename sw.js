202851ba5bc22a6fe48c9098fa8d896cfaf513f0
mansourmahmoud659-afk <mansourmahmoud659@gmail.com>
2026-08-20 15:12:42 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,22 @@
-const CACHE_NAME = 'invoice-gen-v1';
+const CACHE_NAME = 'invoice-pro-v2';
 
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll([
-        '/mahref-vault/',
-        '/mahref-vault/index.html',
-        '/mahref-vault/manifest.json'
-      ]);
-    })
+// مسح الكاش القديم فوراً عند التحديث
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
   );
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => {
-      return response || fetch(e.request);
-    })
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
   );
 });
